4bd8020402149177ad719669c66ccb4077908b24
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-06-20 18:24:45 +0530

    feat: update dependencies and add new UI components
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, and tailwindcss-animate.
    - Updated tailwind-merge to version 2.6.0.
    - Enhanced global styles in globals.css with CSS variables for theming and dark mode support.
    - Created a new HeroSection component with an image and a call-to-action button.
    - Introduced a reusable Button component with variant and size options.
    - Added utility function for class name merging in utils.ts.
    - Updated tailwind.config.ts to support new color variables and added tailwindcss-animate plugin.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import memojiImage from "@/assets/images/memoji-computer.png";
+import { ArrowDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <Image src={memojiImage} alt="Memoji Behind Laptop" />
+        <div>
+          <div></div>
+          <div>Available for work now!</div>
+        </div>
+        <h1>Building exceptional Full Stack Websites</h1>
+        <p>
+          I specialize in creating complete, functional and high performing web
+          applications. Reach out for your next project!
+        </p>
+        <div>
+          <Button variant={"outline"} className="flex items-center gap-2">
+            <span className="text-gray-900">Explore My Work</span>
+            <ArrowDown className="h-6 w-6 text-gray-900" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
