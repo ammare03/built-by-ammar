ff04f360b028123aff7a60ec438846c1f0aecf29
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-06-23 11:30:51 +0530

    feat: implement animated hero section with orbiting elements and fade-in effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import FadeContent from "@/components/bits/FadeContent";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <HeroSection />
+      <FadeContent
+        blur={true}
+        duration={1000}
+        easing="ease-out"
+        initialOpacity={0}
+      >
+        <Header />
+        <HeroSection />
+      </FadeContent>
     </div>
   );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,15 +2,59 @@ import Image from "next/image";
 import memojiImage from "@/assets/images/memoji-computer.png";
 import { ArrowDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import grainImage from "@/assets/images/grain.jpg";
+import { HeroOrbit } from "@/components/HeroOrbit";
+import StarIcon from "@/assets/icons/star.svg";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-16 md:py-24 lg:py-30 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{ backgroundImage: `url(${grainImage.src})` }}
+        />
+        <div className="size-[620px] hero-ring" />
+        <div className="size-[820px] hero-ring" />
+        <div className="size-[1020px] hero-ring" />
+        <div className="size-[1220px] hero-ring" />
+        <HeroOrbit size={800} rotation={-64}>
+          <StarIcon className="size-28 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={10}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={170}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={105}>
+          <div className="size-3 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-2 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-25}>
+          <div className="size-2 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+      </div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
             src={memojiImage}
-            className="size-[100px]"
+            className="size-36"
             alt="Memoji Behind Laptop"
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg animate-pulse">
@@ -18,22 +62,29 @@ export const HeroSection = () => {
             <div className="text-sm font-medium ">Available for work now!</div>
           </div>
         </div>
-        <h1>Building exceptional Full Stack Websites</h1>
-        <p>
-          I specialize in creating complete, functional and high performing web
-          applications. Reach out for your next project!
-        </p>
-        <div>
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional Full Stack Websites
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in creating complete, functional and high performing
+            web applications. Reach out for your next project!
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <Button
             variant={"outline"}
-            className="flex items-center gap-2 border-white bg-transparent text-gray-900 hover:bg-gray-100"
+            className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl text-white bg-transparent hover:bg-white/10 hover:scale-105 transition-all duration-300 z-10"
           >
-            <span className="text-white">Explore My Work</span>
+            <span className="text-white font-semibold">Explore My Work</span>
             <ArrowDown className="h-6 w-6 text-white" />
           </Button>
-          <Button variant={"outline"} className="flex items-center gap-2">
+          <Button
+            variant={"outline"}
+            className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl hover:bg-white/80 hover:scale-105 transition-all duration-300 z-10"
+          >
             <span>👋</span>
-            <span className="text-gray-900">Lets Connect</span>
+            <span className="text-gray-900 font-semibold">Lets Connect</span>
           </Button>
         </div>
       </div>
