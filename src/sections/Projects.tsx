b50334d348e11e0fed828ec1a2f4db1ab4f14539
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-06-23 19:29:27 +0530

    feat: enhance ProjectsSection with improved layout, styling, and new icons

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,6 +4,9 @@ import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import { ArrowUpRightIcon } from "lucide-react";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
@@ -46,33 +49,70 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div>
+    <section className="pb-16 lg:py-24">
       <div className="container">
-        <p>Real-world Results</p>
-        <h2>Featured Projects</h2>
-        <p>See how I transformed concepts into engaging digital experiences</p>
-        <div>
+        <div className="flex justify-center">
+          <p className="text-xl uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+            Real-world Results
+          </p>
+        </div>
+        <h2 className="font-serif text-4xl md:text-6xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4 md:text-lg lg:text-xl max-w-md mx-auto">
+          See how I transformed concepts into engaging digital experiences
+        </p>
+        <div className="flex flex-col mt-10 gap-20 md:mt-20">
           {portfolioProjects.map((project) => (
-            <div key={project.title}>
-              <div>
-                <span>{project.company}</span>
-                <span>{project.year}</span>
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content=[''] after:absolute after:inset-0 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{ backgroundImage: `url(${grainImage.src})` }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase traacking-widest text-sm text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm text-white/50 md:text-base"
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <Link href={project.link}>
+                    <Button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 border border-white/15 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 hover:bg-white/70 transition-colors duration-300 ">
+                      <span className="font-semibold">View Codebase</span>
+                      <ArrowUpRightIcon />
+                    </Button>
+                  </Link>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
               </div>
-              <h3>{project.title}</h3>
-              <hr />
-              <ul>
-                {project.results.map((result, index) => (
-                  <li key={index}>{result.title}</li>
-                ))}
-              </ul>
-              <Link href={project.link} target="_blank">
-                <Button>View Live Site</Button>
-              </Link>
-              <Image src={project.image} alt={project.title} />
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
